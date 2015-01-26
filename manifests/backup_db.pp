f38428bf7df6a6eea4598d2cec26c302fb0ee5df
Neill Turner <neillwturner@gmail.com>
2015-01-26 16:37:19 +0000

    Update backup_db.pp

--- manifests/backup_db.pp
+++ manifests/backup_db.pp
@@ -4,10 +4,13 @@
 #
 #
 class mongodb_mms::backup_db(
-  $logpath           = '/data/backupdb/mongodb.log',
-  $dbpath            = '/data/backupdb',
-  $port              = 27017,
-  $version           = '2.6.4-1')
+  $logpath  = '/var/log/mongodb/mongodb.log',
+  $dbpath   = '/var/lib/mongodb',
+  $dbparent = '/data',
+  $port     = 27017,
+  $version  = '2.6.4-1',)
+#  $user     = 'mongod',
+#  $group    = 'mongod')
 {
 
   class { 'epel': }
@@ -15,22 +18,23 @@ class mongodb_mms::backup_db(
   class { '::mongodb::globals':
     manage_package_repo => true,
     server_package_name => 'mongodb-org',
+    #bind_ip            => ['127.0.0.1','192.168.33.16'],
     bind_ip             => ['0.0.0.0'],
     version             => $version,
     require             => Class['epel']
-  }->
+  }
 
   class {'::mongodb::server':
     auth    => false,
     verbose => true,
-    port    => $port,
     logpath => $logpath,
-    dbpath  => $dbpath,
+    dbpath  => $dbpath,    
+    port    => $port,
     require => Class['::mongodb::globals']
-  }->
+  }
   
- class {'::mongodb::client':
+  class {'::mongodb::client':
     require => Class['::mongodb::server']
   }
-}
 
+}
